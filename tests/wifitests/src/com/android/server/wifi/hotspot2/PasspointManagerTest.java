1b95b16d33c865af6cce274afdcff02011f4691e
Nate Jiang <qiangjiang@google.com>
2020-12-03 14:31:30 -0800

    [Suggestion] Check foreground user for API call
    Also, squashes the follow up commit to create a single CL for
    backporting:
    =======
    PasspointManager: Don't allow bg user to modify passpoint profiles
    Also, add safety net logging for this bug.
    =======
    
    Bug: 174749461
    Test: atest com.android.server.wifi
    Change-Id: Ifc79ffeb04a7be99a9c60d9414b72e88275c0514
    Merged-In: Ifc79ffeb04a7be99a9c60d9414b72e88275c0514
    (cherry picked from commit e799efba85cbe52044a067869af71d9c15b573bb)
    (cherry picked from commit 23685b8604571ec623e539f4f9c66db65c9dde81)
    (cherry picked from commit e26f3b9ffe1c862c8c8322a6ae7b03ce8f9900f9)

--- tests/wifitests/src/com/android/server/wifi/hotspot2/PasspointManagerTest.java
+++ tests/wifitests/src/com/android/server/wifi/hotspot2/PasspointManagerTest.java
@@ -84,6 +84,7 @@ import com.android.server.wifi.hotspot2.anqp.I18Name;
 import com.android.server.wifi.hotspot2.anqp.OsuProviderInfo;
 import com.android.server.wifi.util.InformationElementUtil.RoamingConsortium;
 import com.android.server.wifi.util.ScanResultUtil;
+import com.android.server.wifi.util.WifiPermissionsUtil;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -144,7 +145,7 @@ public class PasspointManagerTest {
     @Mock IProvisioningCallback mCallback;
     @Mock WfaKeyStore mWfaKeyStore;
     @Mock KeyStore mKeyStore;
-
+    @Mock WifiPermissionsUtil mWifiPermissionsUtil;
     TestLooper mLooper;
     PasspointManager mManager;
 
@@ -165,7 +166,8 @@ public class PasspointManagerTest {
         when(mObjectFactory.makePasspointProvisioner(any(Context.class)))
                 .thenReturn(mPasspointProvisioner);
         mManager = new PasspointManager(mContext, mWifiNative, mWifiKeyStore, mClock,
-                mSimAccessor, mObjectFactory, mWifiConfigManager, mWifiConfigStore, mWifiMetrics);
+                mSimAccessor, mObjectFactory, mWifiConfigManager, mWifiConfigStore, mWifiMetrics,
+                mWifiPermissionsUtil);
         ArgumentCaptor<PasspointEventHandler.Callbacks> callbacks =
                 ArgumentCaptor.forClass(PasspointEventHandler.Callbacks.class);
         verify(mObjectFactory).makePasspointEventHandler(any(WifiNative.class),
@@ -498,7 +500,7 @@ public class PasspointManagerTest {
         assertEquals(1, mDataSource.getProviderIndex());
 
         // Remove the provider.
-        assertTrue(mManager.removeProvider(TEST_FQDN));
+        assertTrue(mManager.removeProvider(TEST_CREATOR_UID, TEST_FQDN));
         verify(provider).uninstallCertsAndKeys();
         verify(mWifiConfigManager).saveToStore(true);
         verify(mWifiMetrics).incrementNumPasspointProviderUninstallation();
@@ -538,7 +540,7 @@ public class PasspointManagerTest {
         assertEquals(1, mDataSource.getProviderIndex());
 
         // Remove the provider.
-        assertTrue(mManager.removeProvider(TEST_FQDN));
+        assertTrue(mManager.removeProvider(TEST_CREATOR_UID, TEST_FQDN));
         verify(provider).uninstallCertsAndKeys();
         verify(mWifiConfigManager).saveToStore(true);
         verify(mWifiMetrics).incrementNumPasspointProviderUninstallation();
@@ -658,7 +660,7 @@ public class PasspointManagerTest {
      */
     @Test
     public void removeNonExistingProvider() throws Exception {
-        assertFalse(mManager.removeProvider(TEST_FQDN));
+        assertFalse(mManager.removeProvider(TEST_CREATOR_UID, TEST_FQDN));
         verify(mWifiMetrics).incrementNumPasspointProviderUninstallation();
         verify(mWifiMetrics, never()).incrementNumPasspointProviderUninstallSuccess();
     }
